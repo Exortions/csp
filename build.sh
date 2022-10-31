078c25f83d61e8cad4461364bd9a2710e89d2d0c
Exortions <exortionsmc@gmail.com>
2022-10-30 19:22:59 -0700

    init

---
+++ build.sh
@@ -0,0 +1,1 @@
+/usr/bin/clang++ -fcolor-diagnostics -fansi-escape-codes -g src/main.cpp -o bin/main -I include -I src
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "csp.h"
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Starting csp..." << std::endl;
+    return 0;
+}
